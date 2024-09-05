52388e63bea378dfbf6818b813497a5ef70f15c2
Abhay Shukla <105139789+shuklabhay@users.noreply.github.com>
2024-09-05 00:27:02 -0700

    cleanup signal processing pipeline (#13)

--- src/data_processing/encode_audio_data.py
+++ src/data_processing/encode_audio_data.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.file_helpers import (
     audio_data_dir,
     compiled_data_path,
-    load_loudness_information,
+    load_loudness_data,
 )
 from utils.signal_helpers import encode_sample_directory
 
@@ -19,7 +19,7 @@ else:
 
 encode_sample_directory(audio_data_dir, visualize)
 
-real_data = load_loudness_information(
+real_data = load_loudness_data(
     compiled_data_path
 )  # datapts, channels, frames, freq bins
-print("Data " + str(real_data.shape))
+print("Data shape,", str(real_data.shape))
