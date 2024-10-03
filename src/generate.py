e558694b0dbda9bc1cd236b83bccafd06c592a30
aboombadev <abhayshuklayt@gmail.com>
2024-10-03 14:32:24 -0700

    Update generate.py

--- src/generate.py
+++ src/generate.py
@@ -1,9 +1,7 @@
 from utils.generation_helpers import generate_audio
-from usage_params import (
-    model_to_generate_with,
-    training_sample_length,
-)
+from usage_params import UsageParams
 
 
 # Generate based on usage_params
-generate_audio(model_to_generate_with, training_sample_length, True)
+params = UsageParams()
+generate_audio(params.model_to_generate_with, params.training_sample_length, True)
